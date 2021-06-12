9ae8667c8201df603f12a6dbb162b30e29520070
Afoxys <afoxys10@gmail.com>
2021-06-12 12:22:18 +0100

    Add random traffic

--- src/main.py
+++ src/main.py
@@ -1,8 +1,23 @@
 import os, sys
 import traci
 import traci.constants
+import random
 
 from plans import create_trips, update_beliefs
+
+def print_vehicle_information(vehicle_information):
+    for x in vehicle_information:
+        vehicle_average_speed = 0
+        for y in range(2,len(vehicle_information[x])):
+            vehicle_average_speed += vehicle_information[x][y]
+        vehicle_average_speed = vehicle_average_speed / (len(vehicle_information[x]) - 2)
+        print("Vehicle", x, "  Speed: ", round(vehicle_average_speed,3), " Distance: ", round(vehicle_information[x][0],3), "Time: ", round(vehicle_information[x][0]/vehicle_average_speed,3), "Departure Time: ", round(vehicle_information[x][1],3))
+    
+def add_random_vehicles(traci):
+    for x in range(0,10):
+        for y  in range(0,10):
+            traci.vehicle.add("random"+str((10*x+y)), "route"+str(random.randint(0,2)), "type"+str(random.randint(1,3)), 50 + (10 * x))
+        
 
 # Start SUMO
 if 'SUMO_HOME' in os.environ:
@@ -21,7 +36,7 @@ vehicle_information = {}
 routes_information = {}
 trips = []
     
-max_episodes = 20
+max_episodes = 2
 episode = 1
 
 traci.start(sumoCmd)
@@ -30,13 +45,15 @@ traci.gui.setSchema("View #0", "real world")
     
 while episode <= max_episodes:
 
-    traci.load(['-c', "SUMO/roundabout.sumocfg"])
+    # traci.load(['-c', "SUMO/roundabout.sumocfg"])
     
     edges = traci.edge.getIDList()
     # Start episode 
     print("\nEpisode " + str(episode))
     
     steps = 0
+
+    add_random_vehicles(traci)
 
     # Update beliefs if any
     update_beliefs(episode, trips, vehicle_information)   
@@ -88,31 +105,28 @@ while episode <= max_episodes:
         vehicles=traci.vehicle.getIDList()
         for i in range(0,len(vehicles)): 
             # check if car is in dictionary
-            try:
-                vehicle_information[vehicles[i]]
-            except:
-            # if car is not in dictionary add it with no values
-                vehicle_information[vehicles[i]] = []
-                # vehicle_information[vehicles[i]] = {"time":-1, "distance":-1, "speed":[]}
-                
-            # Update the vehicles information
-            if (len(vehicle_information[vehicles[i]]) == 0):
-                vehicle_information[vehicles[i]].append(traci.vehicle.getDistance(vehicles[i]))
-                vehicle_information[vehicles[i]].append(steps - float(trips[int(vehicles[i])-1]["departureTime"]))
-            vehicle_information[vehicles[i]][0] = traci.vehicle.getDistance(vehicles[i])
-            vehicle_information[vehicles[i]].append(traci.vehicle.getSpeed(vehicles[i]))
+            # print(vehicles[i])
+            if(vehicles[i].find("random") == -1):
+                try:
+                    vehicle_information[vehicles[i]]
+                except:
+                # if car is not in dictionary add it with no values
+                    vehicle_information[vehicles[i]] = []
+                    # vehicle_information[vehicles[i]] = {"time":-1, "distance":-1, "speed":[]}
+                    
+                # Update the vehicles information
+                if (len(vehicle_information[vehicles[i]]) == 0):
+                    vehicle_information[vehicles[i]].append(traci.vehicle.getDistance(vehicles[i]))
+                    vehicle_information[vehicles[i]].append((steps) - float(trips[int(vehicles[i])-1]["departureTime"]))
+                # Update speed and distance travelled 
+                vehicle_information[vehicles[i]][0] = traci.vehicle.getDistance(vehicles[i])
+                vehicle_information[vehicles[i]].append(traci.vehicle.getSpeed(vehicles[i]))
           
         # edges = traci.edge.getIDList()
         # for edge in edges:
         #     print("Number of cars in edge ", edge, ": " ,traci.edge.getLastStepVehicleNumber(edge))
 
-
-    for x in vehicle_information:
-        vehicle_average_speed = 0
-        for y in range(2,len(vehicle_information[x])):
-            vehicle_average_speed += vehicle_information[x][y]
-        vehicle_average_speed = vehicle_average_speed / (len(vehicle_information[x]) - 2)
-        print("Vehicle", x, "  Speed: ", round(vehicle_average_speed,3), " Distance: ", round(vehicle_information[x][0],3), "Time: ", round(vehicle_information[x][0]/vehicle_average_speed,3), "Departure Time: ", round(vehicle_information[x][1],3))
+    print_vehicle_information(vehicle_information)
     
 
     episode = episode + 1
